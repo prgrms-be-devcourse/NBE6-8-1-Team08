b8feadd92fa29a786da827f256ff4890367b142e
LeeKW <lkw9241@gmail.com>
2025-07-14 16:32:02 +0900

    entity

---
+++ backend/src/main/java/com/gridsandcircles/domain/order/orderItems/entity/OrderItems.java
@@ -0,0 +1,8 @@
+package com.gridsandcircles.domain.order.orderItems.entity;
+
+public class OrderItems {
+    private Integer orderItemId;
+    private Integer orderId;
+    private int orderCount;
+    private Integer productId;
+}
