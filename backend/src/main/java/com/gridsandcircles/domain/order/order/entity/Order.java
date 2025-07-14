7f138fc9e3109ee39cf7732274a7bd1ac4a1872f
Kim-You-Seok <luke2062@naver.com>
2025-07-14 22:21:18 +0900

    [BE] feat: Order, OrderItems 엔티티 완성 (#18)
    * [BE] feat: Order, OrderItems 엔티티 추가
    
    * [BE] feat: Order, OrderItems 엔티티 완성
    
    * [BE] fix: 어노테이션 배치 수정
    
    * [BE] fix: 필드 배치 수정
    
    * [BE] fix: 의미없는 공백 제거

--- backend/src/main/java/com/gridsandcircles/domain/order/order/entity/Order.java
+++ backend/src/main/java/com/gridsandcircles/domain/order/order/entity/Order.java
@@ -1,12 +1,44 @@
 package com.gridsandcircles.domain.order.order.entity;
 
-import java.sql.Timestamp;
+import com.gridsandcircles.domain.order.orderItems.entity.OrderItems;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Integer orderId;
+
     private String email;
+
     private String address;
-    private Timestamp createAt;
-    private boolean deliveryStatus;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+
     private boolean orderStatus;
+
+    private boolean deliveryStatus;
+
+    @OneToMany(mappedBy="order", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<OrderItems> orderItems = new ArrayList<>();
+
+    public void addOrderItems(OrderItems items) {
+        orderItems.add(items);
+        items.setOrder(this);
+    }
 }
