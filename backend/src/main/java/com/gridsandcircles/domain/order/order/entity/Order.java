2fd7bbf34e20de6adbd664403e28fc08a66240a5
YouSeok518 <luke2062@naver.com>
2025-07-14 20:19:40 +0900

    [BE] feat: Order, OrderItems 엔티티 추가

--- backend/src/main/java/com/gridsandcircles/domain/order/order/entity/Order.java
+++ backend/src/main/java/com/gridsandcircles/domain/order/order/entity/Order.java
@@ -1,12 +1,43 @@
 package com.gridsandcircles.domain.order.order.entity;
 
-import java.sql.Timestamp;
+import com.gridsandcircles.domain.order.orderItems.entity.OrderItems;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Order {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Integer orderId;
+
     private String email;
+
     private String address;
+
+    @CreationTimestamp
     private Timestamp createAt;
+
     private boolean deliveryStatus;
     private boolean orderStatus;
+
+    @OneToMany(mappedBy="order", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<OrderItems> orderItems = new ArrayList<>();
+
+    public void addOrderItems(OrderItems items) {
+        orderItems.add(items);
+        items.setOrder(this);
+    }
+
 }
--- backend/src/main/java/com/gridsandcircles/domain/order/orderItems/entity/OrderItems.java
+++ backend/src/main/java/com/gridsandcircles/domain/order/orderItems/entity/OrderItems.java
@@ -1,8 +1,27 @@
 package com.gridsandcircles.domain.order.orderItems.entity;
 
+import com.gridsandcircles.domain.order.order.entity.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class OrderItems {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Integer orderItemId;
-    private Integer orderId;
+
     private int orderCount;
+
     private Integer productId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 }
