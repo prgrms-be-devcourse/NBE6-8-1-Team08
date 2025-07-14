11134c9840ce33601aebb946ca94afefa3e4edd8
LeeKW <lkw9241@gmail.com>
2025-07-14 17:13:20 +0900

    entity

--- backend/src/main/java/com/gridsandcircles/domain/product/product/entity/Product.java
+++ backend/src/main/java/com/gridsandcircles/domain/product/product/entity/Product.java
@@ -2,7 +2,7 @@ package com.gridsandcircles.domain.product.product.entity;
 
 public class Product {
     private Integer productId;
-    private String name;
+    private String name; //제품명
     private int price;
     private String description;
     private String productImage;
