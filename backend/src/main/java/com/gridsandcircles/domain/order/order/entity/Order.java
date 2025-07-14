d8ca737bf779625ff07724337307f9a02a217050
lkw9241 <71163228+lkw9241@users.noreply.github.com>
2025-07-14 17:21:16 +0900

    Be/feature/delete (#15)
    * entity
    
    * entity

--- backend/src/main/java/com/gridsandcircles/domain/order/order/entity/Order.java
+++ backend/src/main/java/com/gridsandcircles/domain/order/order/entity/Order.java
@@ -9,5 +9,4 @@ public class Order {
     private Timestamp createAt;
     private boolean deliveryStatus;
     private boolean orderStatus;
-
 }
--- backend/src/main/java/com/gridsandcircles/domain/product/product/entity/Product.java
+++ backend/src/main/java/com/gridsandcircles/domain/product/product/entity/Product.java
@@ -2,12 +2,8 @@ package com.gridsandcircles.domain.product.product.entity;
 
 public class Product {
     private Integer productId;
-
-    private String name;
-
+    private String name; //제품명
     private int price;
-
     private String description;
-
     private String productImage;
 }
