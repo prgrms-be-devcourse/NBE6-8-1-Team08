be9b02d0a9f913d2d17dcec9aea278f628a3abb8
flffkd12 <flffkd12@gmail.com>
2025-07-14 19:15:12 +0900

    [BE] feat: 레포지터리 계층 추가하여 DB 접근

---
+++ backend/src/main/java/com/gridsandcircles/domain/admin/admin/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.gridsandcircles.domain.admin.admin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, String> {
+
+}
