2bc830b230d9ee573ce4b16fdfa4a5a0c70da84c
lkw9241 <71163228+lkw9241@users.noreply.github.com>
2025-07-14 14:43:30 +0900

    folderTree (#10) accepted by dong hyuk

--- backend/src/main/java/com/gridsandcircles/GridsandcirclesApplication.java
+++ backend/src/main/java/com/gridsandcircles/GridsandcirclesApplication.java
@@ -1,4 +1,4 @@
-package com.example.gridsandcircles;
+package com.gridsandcircles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/main/java/com/gridsandcircles/domain/auth/AuthController.java
+++ backend/src/main/java/com/gridsandcircles/domain/auth/AuthController.java
@@ -1,4 +1,4 @@
-package com.example.gridsandcircles.auth;
+package com.gridsandcircles.domain.auth;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
