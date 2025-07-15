68f3de2132902ae78315ea9e7fce44ba3396af68
flffkd12 <flffkd12@gmail.com>
2025-07-15 16:53:36 +0900

    [BE] feat: 로그인 및 인증 모듈 구현
    * 컨트롤러, 서비스, DTO로 구성

--- backend/src/main/java/com/gridsandcircles/domain/auth/AuthController.java
+++ backend/src/main/java/com/gridsandcircles/domain/auth/AuthController.java
@@ -1,15 +1,48 @@
 package com.gridsandcircles.domain.auth;
 
+import com.gridsandcircles.domain.admin.admin.AdminResponseDto;
+import com.gridsandcircles.global.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping
+@RequestMapping("/auth")
 @Tag(name = "AuthController", description = "인증 API")
 public class AuthController {
-  
 
+  private final AuthService authService;
+  private final JwtUtil jwtUtil;
+
+  @PostMapping("login")
+  @Operation(summary = "로그인")
+  public ResponseEntity<ApiResponse<AdminResponseDto>> login(
+      @Valid @RequestBody LoginRequestDto loginRequestDto,
+      HttpServletResponse response
+  ) {
+    String adminId = authService.loginAdmin(loginRequestDto.getAdminId(),
+        loginRequestDto.getPassword());
+    String token = jwtUtil.generateToken(adminId);
+
+    ResponseCookie cookie = ResponseCookie.from("access_token", token)
+        .httpOnly(true)
+        .sameSite("Strict")
+        .path("/")
+        .maxAge(300)
+        .build();
+
+    response.addHeader("Set-Cookie", cookie.toString());
+
+    return ResponseEntity.ok()
+        .body(new ApiResponse<>("Login successful", new AdminResponseDto(adminId)));
+  }
 }
