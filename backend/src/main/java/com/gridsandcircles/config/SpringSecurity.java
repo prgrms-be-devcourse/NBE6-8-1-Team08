67e17da5235e4115855e3c2d1a28ae50f5972e9f
flffkd12 <flffkd12@gmail.com>
2025-07-15 16:35:43 +0900

    [BE] feat: JWT 관련 기능 추가 및 JWT 인증 필터 등록

--- backend/src/main/java/com/gridsandcircles/config/SpringSecurity.java
+++ backend/src/main/java/com/gridsandcircles/config/SpringSecurity.java
@@ -1,5 +1,7 @@
 package com.gridsandcircles.config;
 
+import com.gridsandcircles.domain.auth.JwtAuthenticationFilter;
+import com.gridsandcircles.domain.auth.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,21 +10,26 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SpringSecurity {
 
   @Bean
-  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  SecurityFilterChain filterChain(HttpSecurity http, JwtUtil jwtUtil) throws Exception {
     http
         .csrf(AbstractHttpConfigurer::disable)
         .formLogin(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/h2-console/**", "/",
-                "/admin/signup").permitAll()
+                "/admin/signup", "/auth/login").permitAll()
             .anyRequest().authenticated()
         )
-        .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin));
+        .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
+        .addFilterBefore(
+            new JwtAuthenticationFilter(jwtUtil),
+            UsernamePasswordAuthenticationFilter.class
+        );
 
     return http.build();
   }
