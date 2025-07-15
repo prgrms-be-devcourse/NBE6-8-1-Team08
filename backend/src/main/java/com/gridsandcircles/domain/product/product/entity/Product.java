80fbecf85f678a69fa4477c12629928ab3bf1428
LeeKW <lkw9241@gmail.com>
2025-07-15 13:49:34 +0900

    [BE] feature: orderService, BaseInitData

--- backend/src/main/java/com/gridsandcircles/domain/product/product/entity/Product.java
+++ backend/src/main/java/com/gridsandcircles/domain/product/product/entity/Product.java
@@ -1,6 +1,19 @@
 package com.gridsandcircles.domain.product.product.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="product")
+
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer productId;
     private String name; //제품명
     private int price;
