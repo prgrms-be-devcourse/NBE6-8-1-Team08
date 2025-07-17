d019ca8d18a4dac5c6308197d2cefe61e40341a9
LeeKW <lkw9241@gmail.com>
2025-07-17 15:33:11 +0900

    Controller:removeOrderItem

--- backend/src/main/java/com/gridsandcircles/domain/order/order/controller/ApiV1OrderController.java
+++ backend/src/main/java/com/gridsandcircles/domain/order/order/controller/ApiV1OrderController.java
@@ -5,10 +5,12 @@ import com.gridsandcircles.domain.order.order.entity.Order;
 import com.gridsandcircles.domain.order.order.service.OrderService;
 import com.gridsandcircles.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -36,6 +38,15 @@ public class ApiV1OrderController {
         return new RsData<>(
                 "%d번 주문이 삭제되었습니다.".formatted(id),
                 null
+        );
+    }
+    //브라우저로 작동확인
+    @GetMapping("/{orderId}/items/{orderItemId}")
+    @Transactional
+    public ResponseEntity<RsData<Map<String, Integer>>> removeOrderItem(@PathVariable int orderId, @PathVariable int orderItemId) {
+        orderService.removeOrderItem(orderId, orderItemId);
+        return ResponseEntity.ok(
+                RsData.success("주문 항목 삭제 성공", Map.of("orderId", orderId,"orderItemId", orderItemId))
         );
     }
 }
--- backend/src/main/java/com/gridsandcircles/domain/order/order/service/OrderService.java
+++ backend/src/main/java/com/gridsandcircles/domain/order/order/service/OrderService.java
@@ -4,6 +4,7 @@ import com.gridsandcircles.domain.order.order.entity.Order;
 import com.gridsandcircles.domain.order.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,4 +28,15 @@ public class OrderService {
     public void deleteOrder(Order order){orderRepository.delete(order);}
 
     public Optional<Order> findById(int id) {return orderRepository.findById(id);}
+
+    @Transactional
+    public void removeOrderItem(Integer orderId, Integer orderItemId){
+        Order order = findById(orderId).orElseThrow(()->new RuntimeException("주문을 찾을 수 없습니다."));
+
+        boolean removed = order.getOrderItems().removeIf(item ->item.getOrderItemId().equals(orderItemId));
+
+        if(!removed){
+            throw new RuntimeException("해당 주문 항목을 찾을 수 없습니다.");
+        }
+    }
 }
