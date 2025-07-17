2c5345dc0034daecefc5099d45d7aabe873328ea
DESKTOP-N5KD4EV\litte <littedeer108@gmail.com>
2025-07-17 20:12:54 +0900

    [be] feature : Post 주문등록 기능 구현

--- backend/src/main/java/com/gridsandcircles/domain/order/order/controller/ApiV1OrderController.java
+++ backend/src/main/java/com/gridsandcircles/domain/order/order/controller/ApiV1OrderController.java
@@ -1,15 +1,16 @@
 package com.gridsandcircles.domain.order.order.controller;
 
+import com.gridsandcircles.domain.order.order.dto.OrderCreateReqBody;
+import com.gridsandcircles.domain.order.order.dto.OrderDto;
 import com.gridsandcircles.domain.order.order.dto.OrderResponseDto;
 import com.gridsandcircles.domain.order.order.mapper.OrderMapper;
 import com.gridsandcircles.domain.order.order.service.OrderService;
 import com.gridsandcircles.global.ResultResponse;
+import com.gridsandcircles.global.rsData.RsData;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -28,4 +29,15 @@ public class ApiV1OrderController {
 
         return ResponseEntity.ok().body(new ResultResponse<>("Get order successful", items));
     }
+
+    // 유석님 코드
+    @PostMapping("/{num}")
+    public RsData<OrderDto> createOrder(
+            @PathVariable int num,
+            @Valid @RequestBody OrderCreateReqBody reqBody
+    ){
+        OrderDto orderDto = orderService.registerOrder(num, reqBody);
+        return new RsData<>("201-1", "주문이 성공적으로 등록되었습니다.", orderDto);
+    }
 }
+
