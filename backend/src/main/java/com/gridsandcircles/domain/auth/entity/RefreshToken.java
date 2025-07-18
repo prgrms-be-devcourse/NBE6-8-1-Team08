70ed5e31721ecf0190e7ddff82a86c457e0ea8df
flffkd12 <flffkd12@gmail.com>
2025-07-18 10:04:47 +0900

    [BE] feat: 리프레쉬 토큰 DB 저장

---
+++ backend/src/main/java/com/gridsandcircles/domain/auth/entity/RefreshToken.java
@@ -0,0 +1,19 @@
+package com.gridsandcircles.domain.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class RefreshToken {
+
+  @Id
+  @Column(length = 36)
+  private String refreshToken;
+}
