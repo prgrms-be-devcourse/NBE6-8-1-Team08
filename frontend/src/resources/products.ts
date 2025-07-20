0def3e828c1ddd681e327d3e05fe46393120a358
DESKTOP-N5KD4EV\litte <littedeer108@gmail.com>
2025-07-20 15:24:12 +0900

    [fe] chore: 상품정보

---
+++ frontend/src/resources/products.ts
@@ -0,0 +1,29 @@
+const products = [
+    {
+      PRODUCT_ID: 13,
+      engName: 'Ethiopia Yirgacheffe',
+      price: '4500',
+      imgUrl: 'frontend/src/resources/4.png',
+    },
+    {
+      PRODUCT_ID: 14,
+      engName: 'Columbia Bean',
+      price: '5000',
+      imgUrl: 'frontend/src/resources/4.png',
+    },
+    {
+      PRODUCT_ID: 15,
+      engName: 'Guatemala Antigua',
+      price: '5200',
+      imgUrl: 'frontend/src/resources/3.png',
+    },
+    {
+      PRODUCT_ID: 16,
+      engName: 'Serra do Caparaó',
+      price: '5300',
+      imgUrl: 'frontend/src/resources/4.png',
+    },
+  ];
+  
+  export default products;
+  
